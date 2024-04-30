763696dc2b8b6aa00af7935236bbe8674147cc3a
quokkah <bentanzer9@gmail.com>
2024-04-30 10:40:41 +0200

    base layout (input, accounts)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module console.consolas {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens console.consolas to javafx.fxml;
+    exports console.consolas;
+}
